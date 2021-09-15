956c654da533cc1e573703e4cb71e51523c10610
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-09-15 18:58:03 +0430

    Delete node2D.py

--- node2D.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Node2D:
-    nodeList = []
-    def __init__(self, tag, x, y):
-        self.tag = tag
-        self.x = x
-        self.y = y
-        Node2D.nodeList.append(self)
