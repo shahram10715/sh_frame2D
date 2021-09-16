9de46fc0ab9f82e074459d1fdecd472b83bfbc9b
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-09-16 22:40:06 +0430

    Update solve.py

--- src/solve.py
+++ src/solve.py
@@ -1,20 +1,23 @@
 import numpy as np
+from node2D import *
+from frame2D import *
 
 def solve(nodes, elems, forces, constraints):
+    fix = getFixes(constraints)
     nNodes = len(nodes)
-    nElems = len(elems)
     kglobal = np.zeros((3*nNodes, 3*nNodes))
     for elem in elems:
-        elem.assemble(kglobal)
+        kelem = getStiffMatrix(elem)
+        kglobal = assemble(kglobal, elem, kelem)
     kpart = kglobal
-    kpart = np.delete(kpart, constraints, 0)
-    kpart = np.delete(kpart, constraints, 1)
-    forcesParted = np.delete(forces, constraints)
+    kpart = np.delete(kpart, fix, 0)
+    kpart = np.delete(kpart, fix, 1)
+    forcesParted = np.delete(forces, fix)
     dispVector = np.matmul(np.linalg.inv(kpart), forcesParted)
     dispTemp = np.zeros(3*nNodes)
     j = 0
     for i in range(3*nNodes):
-        if i in constraints:
+        if i in fix:
             pass
         else:
             dispTemp[i] = dispVector[j]
@@ -22,3 +25,4 @@ def solve(nodes, elems, forces, constraints):
     dispVector = dispTemp
     forceVector = np.matmul(kglobal, dispVector)
     return dispVector, forceVector
+
