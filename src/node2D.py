380f03a9af155c1c528029a3fbe5c8a020d680e4
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-09-16 22:39:31 +0430

    Update node2D.py

--- src/node2D.py
+++ src/node2D.py
@@ -8,8 +8,24 @@ class Node2D:
         self.y = y
         Node2D.nodeList.append(self)
 
-    def nodeLoad(self, loads):
-        pass
 
-    def nodConstraint(self, constraints):
-        pass
+def applyNodeLoad(loads, node, nodeLoad):
+    loads[3*node.tag-3] = nodeLoad[0]
+    loads[3*node.tag-2] = nodeLoad[1]
+    loads[3*node.tag-1] = nodeLoad[2]
+    return loads
+
+def applyNodConstraint(constraints, node, nodeConstraint):
+    constraints[3*node.tag-3] = nodeConstraint[0]
+    constraints[3*node.tag-2] = nodeConstraint[1]
+    constraints[3*node.tag-1] = nodeConstraint[2]
+    return constraints
+
+def getFixes(constraints):
+    fix = []
+    for i in range(len(constraints)):
+        if constraints[i] == 1:
+            fix.append(i)
+    return fix
+
+
