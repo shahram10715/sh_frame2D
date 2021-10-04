86175c807502aef197d4b47e63425d42bc4ab4c8
shahram10715 <39901573+shahram10715@users.noreply.github.com>
2021-10-04 12:15:26 +0330

    Update frame2D.py

--- src/frame2D.py
+++ src/frame2D.py
@@ -1,18 +1,21 @@
 import numpy as np
 
+from node2D import Node2D
+
 class Frame2D:
     frame2dList = []
-    def __init__(self, tag, E, A, I, node1, node2, stiffModifier = [1,1,1,1,1,1]):
+    def __init__(self, tag, E, A, I, node1, node2, stiffModifier=[1,1]):
         self.tag = tag
         self.E = E
         self.A = A
         self.I = I
         self.node1 = node1
         self.node2 = node2
-        # a list to modify each DOF which is a list of boolean 0 or 1
-        # [1,1,1,1,1,1] means beam-column element
-        # [0,1,1,0,1,1] means beam element
-        # you can also apply end releases with the stiffModifier
+        # stiffModifier a list to apply end releases
+        # [1,1] means no release
+        # [1,0] means end j release
+        # [0,1] means end i release
+        # [0,0] means truss element
         self.stiffModifier = stiffModifier
         delx = self.node2.x - self.node1.x
         dely = self.node2.y - self.node1.y
@@ -39,12 +42,11 @@ class Frame2D:
 
 
 
-def getStiffMatrix(frame):
+def getLocalStiffMatrix(frame):
     A = frame.A
     E = frame.E
     L = frame.L
     I = frame.I
-    ang = frame.angle
     k1 = A*E/L
     k2 = 12*E*I/L**3
     k3 = 6*E*I/L**2
@@ -64,7 +66,51 @@ def getStiffMatrix(frame):
     ke[4, 4] = k2
     ke[4, 5] = ke[5, 4] = -k3
     ke[5, 5] = k4
+    # applying end releases
+    if frame.stiffModifier == [1,0]:
+        ind1 = [0,1,2,3,4]
+        ind2 = [5]
+    elif frame.stiffModifier == [0,1]:
+        ind1 = [0,1,3,4,5]
+        ind2 = [2]
+    elif frame.stiffModifier == [0,0]:
+        ind1 = [0,1,3,4]
+        ind2 = [2,5]
+    
+    if frame.stiffModifier != [1,1]:
+        k11 = []
+        k22 = []
+        k12 = []
+        k21 = []
+        for i in range(6):
+            for j in range(6):
+                if i in ind1 and j in ind1:
+                    k11.append(ke[i,j])
+                elif i in ind2 and j in ind1:
+                    k21.append(ke[i,j])
+                elif i in ind1 and j in ind2:
+                    k12.append(ke[i,j])
+                elif i in ind2 and j in ind2:
+                    k22.append(ke[i,j])
+        k11 = np.reshape(k11, (len(ind1), len(ind1)))
+        k12 = np.reshape(k12, (len(ind1), len(ind2)))
+        k21 = np.reshape(k21, (len(ind2), len(ind1)))
+        k22 = np.reshape(k22, (len(ind2), len(ind2)))
+        ktemp = np.linalg.inv(k22)
+        ktemp = np.matmul(k12, ktemp)
+        ktemp = np.matmul(ktemp, k21)
+        kc = k11 - ktemp
+        ke = np.zeros((6,6))
+        for i in range(6):
+            for j in range(6):
+                if i in ind1 and j in ind1:
+                    ke[i,j] = kc[ind1.index(i), ind1.index(j)]
+    return ke
 
+
+def getStiffMatrix(frame):
+    ang = frame.angle
+    ke = getLocalStiffMatrix(frame)
     R = np.zeros((6,6))
     R[0, 0] = np.cos(ang)
     R[0, 1] = np.sin(ang)
@@ -79,6 +125,7 @@ def getStiffMatrix(frame):
     ke = np.matmul((R.T), ke)
     ke = np.matmul(ke, R)
     return ke
+
 
 def assemble(kglobal, frame, kelem):
     i = (frame.node1.tag-1)*3
